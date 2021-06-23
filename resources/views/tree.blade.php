4f9a23275d0644c22276d7d10aa70ddbd05d1d9e
Simon Hryszko <simon@hryszko.dev>
2021-06-23 15:35:06 +0200

    Remove redundant

--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -6,19 +6,8 @@
 
 <div class="row">
     <div class="col-10 offset-1 col-md-6 offset-md-3 mt-5">
-      
-      <ul>        
-        @foreach($tree as $t)
-          <li>
-            {{ $t->title }}
-              @if(count($t->childs))
-                @include('child',['childs' => $t->childs])
-              @endif
-          </li>
-        @endforeach
-      </ul>
-
-
-
+      @include('child',['childs' => $tree])
+  </div>
+</div>
 
 @endsection
