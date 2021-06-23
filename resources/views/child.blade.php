c6bd84f15aceb5d669a5f5eb0b3de081c34fc0ad
Simon Hryszko <simon@hryszko.dev>
2021-06-23 15:46:49 +0200

    Change li to bootstrap component

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -1,10 +1,19 @@
-<ul>
     @foreach($childs as $child)
-        <li>
-            {{ $child->title }}
             @if(count($child->childs))
-                @include('child',['childs' => $child->childs])
+
+                <div class="accordion-item">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#tree-{{$child->id}}">
+                        {{$child->title}}
+                    </button>
+                    <div id="tree-{{$child->id}}" class="accordion-collapse collapse">
+                        <div class="accordion-body">
+                            @include('child',['childs' => $child->childs])
+                        </div>
+                    </div>
+                </div>
+            @else
+            <li class="list-group-item">
+                {{$child->title}}
+            </li>
             @endif
-        </li>
     @endforeach
-</ul>
--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -1,12 +1,14 @@
 @extends('common')
 
-@section('title', 'Log in!')
+@section('title', 'Treeview!')
 
 @section('content')
 
 <div class="row">
     <div class="col-10 offset-1 col-md-6 offset-md-3 mt-5">
-      @include('child',['childs' => $tree])
+      <div class="accordion">
+        @include('child',['childs' => $tree])
+    </div>
   </div>
 </div>
 
