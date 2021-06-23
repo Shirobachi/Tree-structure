52438c7332a37ad1bdfb1fdcddbd9a6db63b47aa
Simon Hryszko <simon@hryszko.dev>
2021-06-23 16:58:09 +0200

    Added button to expand all tree's levels

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
 
     <title>@yield('title')</title>
   </head>
@@ -19,8 +22,5 @@
       </div>
       @endif
 		@yield('content')
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
   </body>
 </html>
--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -6,10 +6,24 @@
 
 <div class="row">
     <div class="col-10 offset-1 col-md-6 offset-md-3 mt-5">
+      <button id="toggleAll" class="btn btn-info mb-2 input-block-level form-control" type="button">Toggle all</button>
       <div class="accordion">
         @include('child',['childs' => $tree])
     </div>
   </div>
 </div>
+
+<script>
+  var show = false;
+
+  $('#toggleAll').on('click', function () {
+    if(!show)
+      $('.collapse').collapse('show');
+      else
+        $('.collapse').collapse('hide');
+      
+    show = !show;
+});
+</script>
 
 @endsection
